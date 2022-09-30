501deb19f968c81841c8c840298cd034d817ab63
Guilherme Rodrigues Rosa <ggcodigos@gmail.com>
2022-09-30 09:51:58 -0300

    adding comments

--- JWTGenerator/src/com/jwtgenerator/util/JWTGenerator.java
+++ JWTGenerator/src/com/jwtgenerator/util/JWTGenerator.java
@@ -4,19 +4,25 @@ public class JWTGenerator {
 
 	public static void main(String[] args) {
 		
+		//Secrete used for sign the object with HmacSha256
 		String secret = "ggcodigossecret";
 		
+		//Header of the JWT
 		String header = "{ \"alg\": \"HS256\" }";
+		
+		//body payload of the JWT
 		String payload = "{ \"jwt:nome\": \"João\", \"jwt:admin\": true }";
 		
+		//Header + payload encoded with Base64
 		String encodedData = Base64Url.encode(header) + "." + Base64Url.encode(payload);
 		
+		//the encodedData is signed with the algoritm defined in header		
 		String signature = HmacSha256.sign(secret, encodedData);
 		
+		//The final object jwt its a combination of encodedData with Signature (header.payload.signature)
 		String jwt = encodedData + "." + signature;
 
 		System.out.println(jwt);
 		
-	}
-	
+	}	
 }
